1d98b39cc4b5bf74042f9862766ab9eb8ec1b138
Dennis Gladun <dgl@covis.de>
2019-04-30 15:30:09 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { TooltipModule , BsDropdownModule, AccordionModule, ButtonsModule} from "ngx-bootstrap";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppComponent } from "./app.component";
 import { appRoutingProviders, routing } from './app.routes';
 import { CoreModule } from './core/core.module';
@@ -8,13 +15,17 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, 
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
     CoreModule, 
     routing,
     TooltipModule.forRoot(), 
-    BsDropdownModule.forRoot(), 
-    ButtonsModule.forRoot(),
-    AccordionModule.forRoot()],    
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot()
+  ],  
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
